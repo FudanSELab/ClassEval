493cd15749343743f95b107ff552184e1b7b02bd
Kaixin Wang <67423856+wkx228@users.noreply.github.com>
2023-08-08 00:53:44 +0800

    update setup and requirements

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+import pkg_resources
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ClassEval",
+    py_modules=["ClassEval"],
+    version="1.0",
+    description="",
+    author="FDSE",
+    packages=find_packages(),
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ]
+)
