9a8657b4765cac2a3eeef9860b17f5846e2267cb
Kaixin Wang <67423856+wkx228@users.noreply.github.com>
2023-12-19 16:23:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name="ClassEval",
     py_modules=["ClassEval"],
-    version="1.0",
+    version="1.1.0",
     description="",
     author="FDSE",
     packages=find_packages(),
