aaffabbfe0906e03e708323e488357bf3b5e56a7
Kaixin Wang <67423856+wkx228@users.noreply.github.com>
2024-06-28 13:13:27 +0800

    fix issue #10

--- data/benchmark_solution_code/CalendarUtil.py
+++ data/benchmark_solution_code/CalendarUtil.py
@@ -7,6 +7,7 @@ class CalendarUtil:
     def __init__(self):
         """
         Initialize the calendar with an empty list of events.
+        """
         self.events = []
 
     def add_event(self, event):
